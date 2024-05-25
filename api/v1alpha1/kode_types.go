0c675557e364830eefb7160e8d4542ae37534bf1
Emil Larsson <emil@jacero.se>
2024-05-25 12:25:56 +0200

    cleanup

--- api/v1alpha1/kode_types.go
+++ api/v1alpha1/kode_types.go
@@ -22,9 +22,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // KodeSpec defines the desired state of Kode
 type KodeSpec struct {
 	// Image is the Docker image for code-server
